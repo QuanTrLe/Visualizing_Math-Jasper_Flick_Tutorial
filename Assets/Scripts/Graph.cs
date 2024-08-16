5f798291dcef8b0fe1b7e5cb5274cec51672f92f
QuanTrLe <atl226@lehigh.edu>
2024-08-16 03:27:28 -0400

    Added the option of cycling through graphs with modes of random or in set order. Also added transitioning states between graphs instead of immediately changing hte positions of the cubes

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -14,6 +14,19 @@ public class Graph : MonoBehaviour {
 
 	[SerializeField]
 	FunctionLibrary.FunctionName function; //which functions to use and visualize the graph
+
+	[SerializeField, Min(0f)]
+	float functionDuration = 1f, transitionDuration = 1f; //the duration before cycling to next function
+
+	float duration;
+	bool transitioning;
+
+	FunctionLibrary.FunctionName transitionFunction;
+
+	public enum TransitionMode { Cycle, Random } //for choosing which display mode
+
+	[SerializeField]
+	TransitionMode transitionMode;
 	
     
 	void Awake () {
@@ -30,6 +43,69 @@ public class Graph : MonoBehaviour {
 	}
 
 	void Update()
+	{
+		duration += Time.deltaTime;
+
+		if (transitioning)
+		{
+			if (duration >= transitionDuration) 
+			{
+				duration -= transitionDuration;
+				transitioning = false;
+			}
+		}
+		else if (duration >= functionDuration) {
+			duration -= functionDuration;
+
+			transitioning = true;
+			transitionFunction = function;
+
+			PickNextFunction();
+		}
+
+		if (transitioning) {
+			UpdateFunctionTransition();
+		}
+		else {
+			UpdateFunction();
+		}
+	}
+
+	void PickNextFunction () {
+		if (transitionMode == TransitionMode.Cycle)
+		{
+			function = FunctionLibrary.GetNextFunctionName(function);
+		}
+		else
+		{
+			function = FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+		}
+	}
+
+	void UpdateFunctionTransition () {
+		FunctionLibrary.Function from = FunctionLibrary.GetFunction(transitionFunction);
+		FunctionLibrary.Function to = FunctionLibrary.GetFunction(function);
+
+		float progress = duration / transitionDuration;
+		float time = Time.time;
+		float step = 2f / resolution;
+		float v = 0.5f * step - 1f;
+
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+			if (x == resolution)
+			{
+				x = 0;
+				z += 1;
+				v = (z + 0.5f) * step - 1f;
+			}
+
+			float u = (x + 0.5f) * step - 1f;
+
+			points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
+		}
+	}
+
+	void UpdateFunction()
 	{
 		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function); //which function to use to graph
 
