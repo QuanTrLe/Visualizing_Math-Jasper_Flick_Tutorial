d16e3e2bd87747e0f41a30cf3b01bed590c1bad7
QuanTrLe <atl226@lehigh.edu>
2024-10-03 00:18:17 -0400

    Continued integrating Burst Compilation in with different options and changing the code so that it'll work together using Mathematics libray

--- Assets/Fractal Proj/Fractal.cs
+++ Assets/Fractal Proj/Fractal.cs
@@ -1,10 +1,17 @@
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
+
+using static Unity.Mathematics.math;
+using float3 = Unity.Mathematics.float3;
+using float4x4 = Unity.Mathematics.float4x4;
+using quaternion = Unity.Mathematics.quaternion;
+
 using UnityEngine;
 
 public class Fractal : MonoBehaviour {
 
+    [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
     struct UpdateFractalLevelJob : IJobFor {
         public float spinAngleDelta;
 		public float scale;
@@ -14,20 +21,21 @@ public class Fractal : MonoBehaviour {
 		public NativeArray<FractalPart> parts;
 
         [WriteOnly]
-		public NativeArray<Matrix4x4> matrices;
+		public NativeArray<float4x4> matrices;
 
         public void Execute (int i) {
             FractalPart parent = parents[i / 5];
             FractalPart part = parts[i];
 
             part.spinAngle += spinAngleDelta;
-            part.worldRotation = parent.worldRotation * (part.rotation * Quaternion.Euler(0f, part.spinAngle, 0f)); 
+            part.worldRotation = mul(parent.worldRotation, 
+                mul(part.rotation, quaternion.RotateY(part.spinAngle))); 
             part.worldPosition = parent.worldPosition +
-                parent.worldRotation * (1.5f * scale * part.direction);
+                mul(parent.worldRotation, (1.5f * scale * part.direction));
 
             parts[i] = part;
 
-            matrices[i] = Matrix4x4.TRS(part.worldPosition, part.worldRotation, scale * Vector3.one);
+            matrices[i] = float4x4.TRS(part.worldPosition, part.worldRotation, float3(scale));
         }
     }
 
@@ -43,25 +51,25 @@ public class Fractal : MonoBehaviour {
 	Material material;
 
     //storing some variables for ease of use later when we're creating the fractal
-    static Vector3[] directions = {
-		Vector3.up, Vector3.right, Vector3.left, Vector3.forward, Vector3.back
+    static float3[] directions = {
+		up(), right(), left(), forward(), back()
 	};
 
-	static Quaternion[] rotations = {
-		Quaternion.identity,
-		Quaternion.Euler(0f, 0f, -90f), Quaternion.Euler(0f, 0f, 90f),
-		Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f)
+	static quaternion[] rotations = {
+		quaternion.identity,
+		quaternion.RotateZ(-0.5f * PI), quaternion.RotateZ(0.5f * PI),
+		quaternion.RotateX(0.5f * PI), quaternion.RotateX(-0.5f * PI)
 	};
 
     //for keeping track of all the fractal parts
     struct FractalPart {
-		public Vector3 direction, worldPosition;
-		public Quaternion rotation, worldRotation;
+		public float3 direction, worldPosition;
+		public quaternion rotation, worldRotation;
         public float spinAngle;
 	}
     NativeArray<FractalPart>[] parts; //For each depth level parts
 
-    NativeArray<Matrix4x4>[] matrices;
+    NativeArray<float4x4>[] matrices;
 
     ComputeBuffer[] matricesBuffers;
 
@@ -72,7 +80,7 @@ public class Fractal : MonoBehaviour {
         parts = new NativeArray<FractalPart>[depth];
         //parts[0] = new FractalPart[1]; //because the root level only has one part (the major guy in the middle)
 
-        matrices = new NativeArray<Matrix4x4>[depth];
+        matrices = new NativeArray<float4x4>[depth];
 
         matricesBuffers = new ComputeBuffer[depth];
 		int stride = 16 * 4;
@@ -80,7 +88,7 @@ public class Fractal : MonoBehaviour {
         //populating the rest of the array with the FractalPar structs
         for (int i = 0, length = 1; i < parts.Length; i++, length *= 5) {
             parts[i] = new NativeArray<FractalPart>(length, Allocator.Persistent);
-            matrices[i] = new NativeArray<Matrix4x4>(length, Allocator.Persistent);
+            matrices[i] = new NativeArray<float4x4>(length, Allocator.Persistent);
             matricesBuffers[i] = new ComputeBuffer(length, stride);
         }
 
@@ -121,18 +129,19 @@ public class Fractal : MonoBehaviour {
 
     void Update() {
         //the rotation speed
-        float spinAngleDelta = 22.5f * Time.deltaTime;
+        float spinAngleDelta = 0.125f * PI * Time.deltaTime;
 
         //rotating the root one first of all
         FractalPart rootPart = parts[0][0];
         rootPart.spinAngle += spinAngleDelta;
-        rootPart.worldRotation = 
-            transform.rotation * (rootPart.rotation * Quaternion.Euler(0f, rootPart.spinAngle, 0f));
+        rootPart.worldRotation = mul(transform.rotation,
+            mul(rootPart.rotation, quaternion.RotateY(rootPart.spinAngle)));
         rootPart.worldPosition = transform.position;
 
         parts[0][0] = rootPart;
         float objectScale = transform.lossyScale.x;
-        matrices[0][0] = Matrix4x4.TRS(rootPart.worldPosition, rootPart.worldRotation, Vector3.one);
+        matrices[0][0] = float4x4.TRS(rootPart.worldPosition, 
+            rootPart.worldRotation, float3(objectScale));
         float scale = objectScale;//scale for decreasing size
 
         JobHandle jobHandle = default;
